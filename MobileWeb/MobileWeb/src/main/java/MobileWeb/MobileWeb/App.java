68c8c7d8d7d1f09186494d96bcf4299ad148c26d
seemilre1 <seemilre1@daum.net>
2014-07-10 00:50:28 +0900

    Git - Eclipse

---
+++ MobileWeb/MobileWeb/src/main/java/MobileWeb/MobileWeb/App.java
@@ -0,0 +1,13 @@
+package MobileWeb.MobileWeb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
